ae2f98ea7d5f741d2bf043f69fdb6102b45634b5
djimenez <djimenez@hiberus.com>
2023-07-14 13:30:28 +0200

    Repaso 'Propiedades' actualizado

---
+++ Repaso/Propiedades/Class2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso.Propiedades
+{
+    public class Class2
+    {
+        // Class properties
+        private string? _firstName;
+        private string? _lastName;
+        private int _age;
+
+        // Getters, Setters
+        public string? FirstName
+        {
+            get { return _firstName; } 
+            set { _firstName = value; }
+        }
+
+        public string? LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public int Age
+        {
+            get => _age;
+            set => _age = value;
+        }
+
+    }
+}
---
+++ Repaso/Propiedades/Class3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso.Propiedades
+{
+    public class Class3
+    {
+        // Class properties
+        public string FirstName { get; private set; }
+        public string SecondName { get; init; }
+
+        // Builders
+        public Class3(string firstName, string secondName)
+        {
+            FirstName = firstName;
+            SecondName = secondName;
+        }
+
+        // Not allowed (only in object initializer or builder / init accessor)
+
+        //public void cantChangeSecondName()
+        //{
+        //    SecondName = "dsda";
+        //}
+
+
+    }
+}
